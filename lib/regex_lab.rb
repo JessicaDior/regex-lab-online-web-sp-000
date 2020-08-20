7f453dc8fad13c0f91abf8b46b450311506d1c3e
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-20 00:21:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aAeEiIoOuU]/)
+    return true
+  else return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun|ing\b/)
 
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
 
 end
 
@@ -15,5 +20,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+  if phone.scan(/(\d+)-(\d+)-(\d+)/)
+    return true
+  else return false
+  end
 
 end
